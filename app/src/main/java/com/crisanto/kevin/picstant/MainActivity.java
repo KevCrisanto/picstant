4b09f61fd489789da3e0fabc8fd53af080f7a041
Kevin <kevincrisanto@gmail.com>
2018-04-13 23:20:44 -0700

    Redesigned nav header and added listener

--- app/src/main/java/com/crisanto/kevin/picstant/MainActivity.java
+++ app/src/main/java/com/crisanto/kevin/picstant/MainActivity.java
@@ -1,26 +1,51 @@
 package com.crisanto.kevin.picstant;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     DrawerLayout mDrawerLayout;
     ActionBarDrawerToggle mActionDrawerToggle;
+    NavigationView mNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Layout variables
         mDrawerLayout = findViewById(R.id.main_drawer_layout);
+        mNavigationView = findViewById(R.id.main_nav_view);
         mActionDrawerToggle = new ActionBarDrawerToggle( this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mActionDrawerToggle);
         mActionDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // listener for navigation view
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.profile) {
+                    Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_LONG).show();
+                    return true;
+                } else if(item.getItemId() == R.id.settings) {
+                    Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_LONG).show();
+                    return true;
+                } else if(item.getItemId() == R.id.logout) {
+                    Toast.makeText(MainActivity.this, "Logout", Toast.LENGTH_LONG).show();
+                    return true;
+                } else{
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+        });
     }
 
     @Override
