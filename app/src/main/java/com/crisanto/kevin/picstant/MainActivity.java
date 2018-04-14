c4dda7351e432e9c63e2afcc8134444db2d89926
Kevin <kevincrisanto@gmail.com>
2018-04-13 22:20:40 -0700

    Designed the navigation drawer

--- app/src/main/java/com/crisanto/kevin/picstant/MainActivity.java
+++ app/src/main/java/com/crisanto/kevin/picstant/MainActivity.java
@@ -1,13 +1,33 @@
 package com.crisanto.kevin.picstant;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mActionDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.main_drawer_layout);
+        mActionDrawerToggle = new ActionBarDrawerToggle( this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mActionDrawerToggle);
+        mActionDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mActionDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
