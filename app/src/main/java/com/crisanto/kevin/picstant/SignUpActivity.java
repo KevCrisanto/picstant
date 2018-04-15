afeaf8758f06414dfdd66d8fb64bcb6d151a08d6
Kevin <kevincrisanto@gmail.com>
2018-04-14 19:09:51 -0700

    Designed sign up screen

--- app/src/main/java/com/crisanto/kevin/picstant/SignUpActivity.java
+++ app/src/main/java/com/crisanto/kevin/picstant/SignUpActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     LinearLayout mLoginContainer;
     AnimationDrawable mAnimationDrawable;
@@ -13,16 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
         // Configure animation
         mLoginContainer = (LinearLayout) findViewById(R.id.login_container);
         mAnimationDrawable = (AnimationDrawable) mLoginContainer.getBackground();
         mAnimationDrawable.setEnterFadeDuration(5000);
         mAnimationDrawable.setExitFadeDuration(2000);
-
     }
-
     // Start animation
     @Override
     protected void onResume() {
@@ -41,3 +39,4 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 }
+
