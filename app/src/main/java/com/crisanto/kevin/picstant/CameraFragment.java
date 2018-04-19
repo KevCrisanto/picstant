50926216da41ef63c4fdd09ae5a968c4c82d7a5e
Kevin <kevincrisanto@gmail.com>
2018-04-18 17:22:38 -0700

    Added methods to upload images to server

--- app/src/main/java/com/crisanto/kevin/picstant/CameraFragment.java
+++ app/src/main/java/com/crisanto/kevin/picstant/CameraFragment.java
@@ -1,9 +1,13 @@
 package com.crisanto.kevin.picstant;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -11,15 +15,33 @@ import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class CameraFragment extends Fragment {
@@ -29,6 +51,8 @@ public class CameraFragment extends Fragment {
     Uri mImageUri;
     final int CAPTURE_IMAGE = 1;
     Bitmap bitmap;
+    String mStoryTitle, imageToString, mProfileImage;
+    boolean OkToUpload;
 
     public CameraFragment() {
         // Required empty public constructor
@@ -49,12 +73,17 @@ public class CameraFragment extends Fragment {
         upload_btn = (Button) view.findViewById(R.id.upload_btn);
         capture_btn = (Button) view.findViewById(R.id.capture_btn);
         captured_iv = (ImageView) view.findViewById(R.id.captured_iv);
+
+        OkToUpload = false;
+
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        getProfileImage();
 
         capture_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,6 +122,7 @@ public class CameraFragment extends Fragment {
                         bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), mImageUri);
 
                         if(bitmap != null){
+                            OkToUpload = true;
                             captured_iv.setImageBitmap(bitmap);
                             Log.i("bitmap", bitmap.toString());
                         }
@@ -105,7 +135,159 @@ public class CameraFragment extends Fragment {
     }
 
     private void storyAndImageTitle() {
+        final EditText editText = new EditText(getContext());
+        editText.setTextColor(Color.BLACK);
+        editText.setHint("Set title/tags for story");
+        editText.setHintTextColor(Color.GRAY);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Story Title");
+        builder.setCancelable(false);
+        builder.setView(editText);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mStoryTitle = editText.getText().toString();
+                imageToString = convertImageToString();
+                uploadStory();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void getProfileImage() {
+
+        User user = SharedPreferenceManager.getInstance(getContext()).getUserData();
+        int user_id = user.getId();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLS.get_user_data+user_id, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    if(!jsonObject.getBoolean("error")){
+                        JSONObject jsonObjectUser = jsonObject.getJSONObject("user");
+
+                        mProfileImage = jsonObjectUser.getString("image");
+
+                    } else{
+                        Toast.makeText(getContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                    }
+
+                } catch(JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+        VolleyHandler.getInstance(getContext()).addRequestToQueue(stringRequest);
+    }
+
+    private void uploadStory() {
+
+        if(!OkToUpload){
+            Toast.makeText(getContext(), "There is no image to upload.", Toast.LENGTH_LONG).show();
+            return;
+        }
+        final String dateOfImage = dateOfImage();
+        final String currentTime = currentReadableTime();
+        User user = SharedPreferenceManager.getInstance(getContext()).getUserData();
+        final String username = user.getUsername();
+        final int user_id = user.getId();
+        final String profile_image = mProfileImage;
+
+        final ProgressDialog mProgressDialog = new ProgressDialog(getContext());
+        mProgressDialog.setTitle("Log In");
+        mProgressDialog.setMessage("Please wait...");
+        mProgressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URLS.upload_story_image,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            if (!jsonObject.getBoolean("error")) {
+                                mProgressDialog.dismiss();
+                                JSONObject jsonObjectUser = jsonObject.getJSONObject("image");
+
+                                Toast.makeText(getContext(), jsonObject.getString("Story uploaded successfully"), Toast.LENGTH_LONG).show();
+                                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                                ft.replace(R.id.main_fragment_content, new HomeFragment());
+                                ft.commit();
+
+                            } else {
+                                Toast.makeText(getContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                        mProgressDialog.dismiss();
+                    }
+                }
+        ) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> imageMap = new HashMap<>();
+                imageMap.put("image_name", username + "-" + dateOfImage);
+                imageMap.put("image_encoded", imageToString);
+                imageMap.put("title", mStoryTitle);
+                imageMap.put("time", currentTime);
+                imageMap.put("username", username);
+                imageMap.put("user_id", String.valueOf(user_id));
+                imageMap.put("profile_image", profile_image);
+                return imageMap;
+            }
+
+        }; // end of StringRequest
+
+        VolleyHandler.getInstance(getContext().getApplicationContext()).addRequestToQueue(stringRequest);
+    }
+
+    private String dateOfImage(){
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        timestamp.toString();
+        return timestamp.toString();
+    }
+
+    private String currentReadableTime(){
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        Date date = new Date(timestamp.getTime());
+        return date.toString();
+    }
+
+    private String convertImageToString(){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageByteArray = baos.toByteArray();
+        String result = Base64.encodeToString(imageByteArray, Base64.DEFAULT);
+        return result;
     }
 
 }
