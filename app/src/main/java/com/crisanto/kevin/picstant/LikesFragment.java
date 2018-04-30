b615d9e33f4faa14b3afa79a3e69139548aa97dc
Kevin <kevincrisanto@gmail.com>
2018-04-29 23:57:37 -0700

    Added code to display all the images that the user has liked

--- app/src/main/java/com/crisanto/kevin/picstant/LikesFragment.java
+++ app/src/main/java/com/crisanto/kevin/picstant/LikesFragment.java
@@ -1,14 +1,34 @@
 package com.crisanto.kevin.picstant;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.crisanto.kevin.picstant.models.Like;
+import com.crisanto.kevin.picstant.models.User;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class LikesFragment extends Fragment {
+
+    ListView likes_lv;
+    ArrayList<Like> arrayLikesList;
+    LikeArrayAdapter likeArrayAdapter;
+    ProgressDialog mProgressDialog;
 
     public LikesFragment() {
         // Required empty public constructor
@@ -24,7 +44,114 @@ public class LikesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_likes, container, false);
+        View view = inflater.inflate(R.layout.fragment_likes, container, false);
+
+        likes_lv = view.findViewById(R.id.likes_lv);
+        arrayLikesList = new ArrayList<Like>();
+        likeArrayAdapter = new LikeArrayAdapter(getContext(), R.layout.like_single_item, arrayLikesList);
+
+        likes_lv.setAdapter(likeArrayAdapter);
+
+        mProgressDialog = new ProgressDialog(getContext());
+        mProgressDialog.setTitle("Loading feed");
+        mProgressDialog.setMessage("Loading the stories you liked...");
+
+        // get the id of all the stories that the user has liked
+        getAllStoryIds();
+        // get data from stories
+
+        return view;
+    }
+
+    private void getAllStoryIds(){
+
+        User user = SharedPreferenceManager.getInstance(getContext()).getUserData();
+        int user_id = user.getId();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLS.get_all_story_ids+user_id, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    if(!jsonObject.getBoolean("error")){
+                        JSONArray jsonArrayIds = jsonObject.getJSONArray("ids");
+                        Log.i("arrayids", jsonArrayIds.toString());
+
+                        String ids = jsonArrayIds.toString();
+                        ids = ids.replace("[", "");
+                        ids = ids.replace("]", "");
+
+                        if(ids == null || ids.isEmpty()) {
+
+                        } else{
+                            getAllStoriesThatWeLiked(ids);
+                        }
+
+                    } else{
+                        Toast.makeText(getContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                    }
+
+                } catch(JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+        VolleyHandler.getInstance(getContext().getApplicationContext()).addRequestToQueue(stringRequest);
+    }
+
+    private void getAllStoriesThatWeLiked(String ids){
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLS.all_stories_we_liked+ids, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    if(!jsonObject.getBoolean("error")){
+                        mProgressDialog.dismiss();
+                        JSONArray jsonArrayStories = jsonObject.getJSONArray("stories");
+                        //JSONObject jsonObjectStories = jsonObject.getJSONObject("stories");
+                        //JSONArray jsonArrayBigArray = jsonObjectStories.getJSONArray("big array");
+
+                        for (int i = 0; i < jsonArrayStories.length(); i++) {
+                            JSONObject jsonObjectSingleStory= jsonArrayStories.getJSONObject(i);
+
+                            Like like = new Like(jsonObjectSingleStory.getInt("id"), jsonObjectSingleStory.getString("image_url"),
+                                    jsonObjectSingleStory.getString("username"));
+                            arrayLikesList.add(like);
+                        }
+                        likeArrayAdapter.notifyDataSetChanged();
+
+                    } else{
+                        Toast.makeText(getContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                    }
+
+                } catch(JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                        mProgressDialog.dismiss();
+                    }
+                }
+        );
+
+        VolleyHandler.getInstance(getContext().getApplicationContext()).addRequestToQueue(stringRequest);
     }
 
 }
